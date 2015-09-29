0af1da3b65af2c461dbf0ff0f656d5d27296ba88
James Hinton <james.hinton@uqconnect.edu.au>
2015-09-29 19:47:55 +1000

    Updated

--- postgres.py
+++ postgres.py
@@ -24,38 +24,36 @@ class database_methods(object):
                 conn.close()
                 print("Database Connection Closed")
 
-    def table_create(tablename):
-        print(tablename)
+    def table_create(self):
         try:
-            conn = psycopg2.connect("host='localhost' dbname='James' user='James' password='i0cy7r9g'")
-            print("Table Name: {0}".format(tablename))
+            conn = psycopg2.connect("host='localhost' dbname='stock_data' user='James' password='i0cy7r9g'")
             cursor = conn.cursor()
-            SQL = "CREATE TABLE {0} (Id INTEGER PRIMARY KEY, Adj_Close FLOAT, Init_Close FLOAT, Close_Date DATE, High_P FLOAT, Low_P FLOAT , Open_P FLOAT , Symbol VARCHAR(8), Volume INT );".format(tablename)
-            print(SQL)
+            SQL = "CREATE TABLE {0} (Id BIGSERIAL PRIMARY KEY, Adj_Close FLOAT, Init_Close FLOAT, Close_Date DATE, High_P FLOAT, Low_P FLOAT , Open_P FLOAT , Symbol VARCHAR(8), Volume INT );".format(self)
             cursor.execute(SQL)
             conn.commit()
+            print("Database Opened")
         except psycopg2.DatabaseError as e:
             print ("Error {0}", format(e))
             sys.exit(1)
         finally:
             if conn:
                 conn.close()
-                print("Database Connection Closed")
+
 
     def write_database(self, SQL):
         try:
-            conn = psycopg2.connect("host='localhost' dbname='{0}' user='James' password='i0cy7r9g'".format(self))
-            print("Database Opened")
+            conn = psycopg2.connect("host='localhost' dbname='stock_data' user='James' password='i0cy7r9g'")
             cursor=conn.cursor()
             cursor.execute(SQL)
             conn.commit()
         except psycopg2.DatabaseError as e:
             print ("Error {0}", format(e))
             sys.exit(1)
-        finally:
-            if conn:
-                conn.close()
-                print("Database Connection Closed")
+
+    def close_database(self):
+        conn = psycopg2.connect("host='localhost' dbname='stock_data' user='James' password='i0cy7r9g'")
+        conn.close()
+        print("Database Connection Closed")
 
 
 if __name__ == "__main__":
--- yahoo_finance_requests.py
+++ yahoo_finance_requests.py
@@ -22,4 +22,16 @@ class pull_data:
             SQL="INSERT INTO test_stock (adj_close, init_close, close_date,high_p,low_p,open_p,symbol,volume) VALUES ({0},{1},'{2}',{3},{4},{5},'{6}',{7});".format(stock[i]['Adj_Close'],stock[i]['Close'],formatted_date,stock[i]['High'],stock[i]['Low'],stock[i]['Open'],stock[i]['Symbol'],stock[i]['Volume'])
             print(SQL)
 
+    def store_data(self,stock_code,start_date,end_date):
+        stock=Share(stock_code).get_historical(start_date,end_date)
+        print(stock)
+        postgres.database_methods.table_create(self)
+        for i in range(0, len(stock)):
+            #SQL="Close was {0}, Open was {1}, Symbol is {2}".format(stock[i]['Close'],stock[i]['Open'],stock[i]['Symbol'])
+            formatted_date = datetime.strptime(stock[i]['Date'].replace("-"," "), '%Y %m %d')
+            SQL="INSERT INTO {8} (adj_close, init_close, close_date,high_p,low_p,open_p,symbol,volume) VALUES ({0},{1},'{2}',{3},{4},{5},'{6}',{7});".format(stock[i]['Adj_Close'],stock[i]['Close'],formatted_date,stock[i]['High'],stock[i]['Low'],stock[i]['Open'],stock[i]['Symbol'],stock[i]['Volume'],self)
+            print(SQL)
+            postgres.database_methods.write_database(self,SQL)
+        postgres.database_methods.close_database('stock_data')
+
 
